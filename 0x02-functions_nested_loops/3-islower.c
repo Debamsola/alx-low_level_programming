0fd127f6ef7efba2a3d65e9df1ad9fad27e53027
Debamsola <stephenola39@gmail.com>
2022-10-18 10:12:17 -0700

    commit 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *int _islower(int c) - a function that checks for lowercase character.
+ */
+
+int _islower(int c)
+{
+	
+		if (c >= 'a' && c <= 'z' && c++)
+			return (1);
+		else
+			return (0);
+}
