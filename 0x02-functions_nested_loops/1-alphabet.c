0cd85b038757a56cf60531ccc566a39cb4b6f458
Debamsola <stephenola39@gmail.com>
2022-10-18 08:59:10 -0700

    commit 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,11 @@
  *  Return: ALwys 0.
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
-	return (0);
 }
