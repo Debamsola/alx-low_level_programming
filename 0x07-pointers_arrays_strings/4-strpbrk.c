6acf0235f96d4167827ba178149520df29e428fc
Debamsola <stephenola39@gmail.com>
2022-10-31 07:39:05 -0700

    commit 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - searches a string for any of a set of bytes.
+ * @s: string.
+ * @accept: bytes.
+ * Return: Null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	if (s == NULL || accept == NULL)
+	{
+		return (NULL);
+	}
+	while (*s)
+	{
+		if (_strchr(accept, *s))
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
