2969d3dcd125f1c2be319dd282fc36818c1cfb77
Debamsola <stephenola39@gmail.com>
2022-11-03 14:09:59 -0700

    commit last

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_prime -wrapper function to check for prime.
+ * @n: input number.
+ * @i: factor to check if n is divisible.
+ * Return: 1 if prime, 0 if not else recurse.
+ */
+
+int is_prime(int n, int i)
+{
+	if (i == 1)
+
+	if (n % i == 0)
+		return (0);
+	return (0);
+}
